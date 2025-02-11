f83a1814478ba4641973dec08e6dceb79164b5bb
jetta <jettaphoom@gmail.com>
2025-02-11 10:51:02 +0700

    [update] fix ui

--- app/components/modal/LoginModal.tsx
+++ app/components/modal/LoginModal.tsx
@@ -57,10 +57,7 @@ const LoginModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading
-        title="Welcome back"
-        subtitle="Login to your account! (as admin: admin@email.com, 12345)"
-      />
+      <Heading title="Welcome back" subtitle="Login to your account!" />
       <Input
         id="email"
         label="Email"
